58df1136adf8a1f916efbf9ec1cbb9b57d5345a9
Walid VM <walid.hanifi707@gmail.com>
2024-09-07 13:27:27 +1000

    fix: existing implementation

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import { getPlaceAutocomplete } from './maps-api'
 
 export async function getAutoCompleteDetails(address: any): Promise<any> {
   const apiKey = process.env.TOMTOM_API_KEY
-  // get autocomplete results
-  const res = getPlaceAutocomplete(process.env.TOMTOM_API_KEY, address).then(async (autocompleteResults) => {
-    const res = []
+
+  try {
+    const res = await getPlaceAutocomplete(apiKey, address)
     return res
-  })
-  // loop over and get details and map results
-  return res
+  } catch (err) {
+    console.error(err)
+  }
 }
--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,16 +1,29 @@
 import axios from 'axios'
 
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
-export async function getPlaceAutocomplete(key: string, address: string) {
-  const autocomplete = await axios.get(`https://api.tomtom.com/search/2/search/${address}.json'`, {
-    params: {
-      key,
-      limit: 100,
-    },
-  })
-  return autocomplete.data.results.map((result) => {
-    return {
-      placeId: result.id,
+export async function getPlaceAutocomplete(
+  key: string,
+  address: string
+) {
+  const autocomplete = await axios.get(
+    `https://api.tomtom.com/search/2/search/${address}.json'`,
+    {
+      params: {
+        key,
+        limit: 100,
+      },
     }
+  )
+
+  return autocomplete.data.results.map((result) => {
+    const response = {
+      placeId: result.id,
+      streetNumber: result.address.streetNumber || '',
+      countryCode: result.address.countryCode,
+      country: result.address.country,
+      freeformAddress: result.address.freeformAddress,
+      municipality: result.address.municipality
+    }
+    return response
   })
 }
