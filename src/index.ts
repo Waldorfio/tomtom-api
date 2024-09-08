a3acc81a1b77f000e547261d6a4802e0e4b93836
Walid VM <walid.hanifi707@gmail.com>
2024-09-08 12:25:54 +1000

    feat: create config to safely load .env

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
+import { tomtomConfig } from './config'
 import { getPlaceAutocomplete } from './maps-api'
 
 export async function getAutoCompleteDetails(address: any): Promise<any> {
-  const apiKey = process.env.TOMTOM_API_KEY
-  const apiVer = process.env.TOMTOM_API_VERSION
-
-  if (!apiKey || !apiVer) {
-    throw new Error(`Missing environment variables: ${!apiKey ? 'API Key' : ''}${!apiKey && !apiVer ? ' and ' : ''}${!apiVer ? 'API Version' : ''}`);
-  }
-
+  const config = tomtomConfig()
   try {
-    const res = await getPlaceAutocomplete(apiKey, apiVer, address)
+    const res = await getPlaceAutocomplete(config.apiKey, config.apiVer, address)
     return res
   } catch (err) {
     console.error(err)
--- test/maps-search.test.ts
+++ test/maps-search.test.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import { tomtomConfig } from '../src/config'
 import { config } from 'dotenv'
 import { describe } from '@jest/globals'
 import { getPlaceAutocomplete } from '../src/maps-api'
@@ -9,12 +9,11 @@ config()
 
 // These are end-to-end tests and need an api key
 describe('Tomtom Places E2E Tests', () => {
-  const apiKey = process.env.TOMTOM_API_KEY
-  const apiVer = process.env.TOMTOM_API_VERSION
+  const config = tomtomConfig()
 
   beforeAll(() => {
-    if (!apiKey || !apiVer) {
-      throw new Error(`Missing environment variables: ${!apiKey ? 'API Key' : ''}${!apiKey && !apiVer ? ' and ' : ''}${!apiVer ? 'API Version' : ''}`);
+    if (!config.apiKey || !config.apiVer) {
+      throw new Error(`Missing environment variables: ${!config.apiKey ? 'API Key' : ''}${!config.apiKey && !config.apiVer ? ' and ' : ''}${!config.apiVer ? 'API Version' : ''}`);
     }
   })
 
@@ -39,20 +38,20 @@ describe('Tomtom Places E2E Tests', () => {
 
   describe('getPlaceAutocomplete', () => {
     it('handles no results', async () => {
-      const res = await getPlaceAutocomplete(apiKey, apiVer, 'asfasffasfasafsafs')
+      const res = await getPlaceAutocomplete(config.apiKey, config.apiVer, 'asfasffasfasafsafs')
       expect(res).toStrictEqual([])
     })
 
     it('throws error on invalid api key', async () => {
-      expect(getPlaceAutocomplete('invalidapikey', apiVer, 'Charlotte Street')).rejects.toThrow()
+      expect(getPlaceAutocomplete('invalidapikey', config.apiVer, 'Charlotte Street')).rejects.toThrow()
     })
 
     it('throws error on invalid api version', async () => {
-      expect(getPlaceAutocomplete(apiKey, '0', 'Charlotte Street')).rejects.toThrow()
+      expect(getPlaceAutocomplete(config.apiKey, '0', 'Charlotte Street')).rejects.toThrow()
     })
 
     it('only returns Australian addresses', async () => {
-      const res = await getPlaceAutocomplete(apiKey, apiVer, 'Charlotte Street')
+      const res = await getPlaceAutocomplete(config.apiKey, config.apiVer, 'Charlotte Street')
 
       expect(Array.isArray(res)).toBe(true)
       if (Array.isArray(res)) {
@@ -68,7 +67,7 @@ describe('Tomtom Places E2E Tests', () => {
     })
 
     it('handles error', async () => {
-      expect(getPlaceAutocomplete(apiKey, apiVer, '')).rejects.toThrow()
+      expect(getPlaceAutocomplete(config.apiKey, config.apiVer, '')).rejects.toThrow()
     })
   })
 })
